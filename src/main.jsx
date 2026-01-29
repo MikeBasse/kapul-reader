0b0e16fdead078bd1de9122309f5f1572e2fd3a1
MikeBasse <132737988+MikeBasse@users.noreply.github.com>
2026-01-29 21:42:54 +1000

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import KapulReaderApp from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <KapulReaderApp />
-  </React.StrictMode>,
-)
